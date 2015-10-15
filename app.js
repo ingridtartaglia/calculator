7df4e114b6e5f9bc72c72a148fb7f076402d80bc
Ingrid Tartaglia <ingrid.tartaglia@gmail.com>
2015-10-15 00:09:34 -0300

    angular

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 var calculator = angular.module('calculator', []);
 
 calculator.controller('calculatorCtrl', function($scope){
+    $scope.firstNumber = 0;
+    $scope.secondNumber = 0;
+    $scope.operation;
+    $scope.result = 0;
+
+    $scope.clickNumber = function(number) {
+        $scope.firstNumber = $scope.firstNumber * 10 + number;
+        $scope.result = $scope.firstNumber;
+    }
 
 });
