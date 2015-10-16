eaf9b401d7bbc2e6be59c16ce674c1dc203c218b
ingridtartaglia <ingrid.tartaglia@gmail.com>
2015-10-16 10:58:58 -0300

    js

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ var calculator = angular.module('calculator', []);
 
 calculator.controller('calculatorCtrl', function($scope){
     $scope.firstNumber = 0;
-    $scope.secondNumber = 0;
+    $scope.secondNumber = null;
     $scope.operation;
     $scope.result = 0;
 
     $scope.clickNumber = function(number) {
         $scope.firstNumber = $scope.firstNumber * 10 + number;
         $scope.result = $scope.firstNumber;
+    }
+
+    $scope.clickOperation = function(operation) {
+        $scope.operation = operation;
     }
 
 });
